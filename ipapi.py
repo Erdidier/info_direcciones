bb939164986154767dd2fa18b9a059b8606a3df5
Erdidier <48653966+Erdidier@users.noreply.github.com>
2021-07-29 12:35:39 -0500

    Modificación ipapi

--- ipapi.py
+++ ipapi.py
@@ -1,13 +1,16 @@
 """
 Creado: Sábado Julio 17 20:04 2021
 @author: Erick Didier Hernández Victoriano
-Última modificación: Miércoles Julio 21 19:14 2021
+Última modificación: Jueves Julio 29 12:23 2021
 """
 
 from requests import get
 
-ip = input("Ingrese la dirección IP pública que desea analizar: ")
+def ipapi():
+    ip = input("Ingrese la dirección IP pública que desea analizar: ")
 
-loc = get('https://ipapi.co/'+ip+'/json/')
-dic_ip = loc.json()
-print (dic_ip)
+    loc = get('https://ipapi.co/'+ip+'/json/')
+    dic_ip = loc.json()
+    return dic_ip
+
+print(ipapi())
